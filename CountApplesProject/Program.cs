e1d74090c60201f70cd381f94908b8fc9c87f5e4
unknown <martin.nilsson10@R9ZBVLA.learnet.se>
2013-09-17 08:58:43 +0200

    I have made a start for the CountApples project

---
+++ CountApplesProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+    
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("HOW MANY APPLES DO WE HAVE TOGETHER?\n");
+
+        string personOne = "Martin";
+        int martinApples = 7;
+
+        // TODO: Classmate must add his / hers name and how many apples he / she have
+        string personTwo = "PersonTwo"; 
+        int personTwoApples = 0;
+
+        // TODO: Classmate must add his / hers name and how many apples he / she have
+        string personThree = "PersonThree";
+        int personThreeApples = 0;
+
+
+        int totalApples = martinApples + personTwoApples +  personThreeApples;
+
+        Console.WriteLine("My name is " + personOne + " and I have " + martinApples + " apples.");
+        Console.WriteLine("My name is " + personTwo + " and I have " + personTwoApples + " apples.");
+        Console.WriteLine("My name is " + personThree + " and I have " + personThreeApples + " apples.");
+
+        Console.WriteLine("\nTogether we have " + totalApples + " apples. That will be one hell of an apple pie!!!");
+        
+
+    }
+}
